fe5a6339fc2fc610b01c55d0691765c5d5aba132
Justin Meyer <justinbmeyer@gmail.com>
2016-12-01 21:29:21 -0600

    fixes #2

--- package.js
+++ package.js
@@ -5,6 +5,16 @@ module.exports = {
     add: function(line, curData, scope, objects, currentWrite) {
         var pkgPath = path.join(path.dirname(this.src.path), line.replace("@package", "").trim());
         var pkg = fs.readFileSync(pkgPath).toString();
-		curData.package = JSON.parse(pkg);
+        var result = JSON.parse(pkg);
+
+        // delete things that would be too big
+        delete result.readme;
+        // or things that don't matter
+        for(var prop in result) {
+            if(prop[0] === "_") {
+                delete result[prop];
+            }
+        }
+		curData.package = result;
     }
 };
