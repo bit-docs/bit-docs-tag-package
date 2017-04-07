04a3b429179325d7c3497aa10ba07888424a37fa
Joel Kuzmarski <leoj3n@gmail.com>
2017-04-07 07:59:39 -0500

    prelim docs

--- bit-docs.js
+++ bit-docs.js
@@ -1,5 +1,17 @@
 var pkg = require("./package");
 
+/**
+ * @module {function} bit-docs-tag-package
+ * @parent plugins
+ *
+ * @description Adds a `@package` tag for use in `bit-docs`.
+ *
+ * @body
+ *
+ * `@package` will add the package's `package.json` info to the `docObject`.
+ *
+ * Use: `@pacakge ../../package.json`
+ */
 module.exports =  function(bitDocs) {
     bitDocs.register("tags",{
     	package: pkg
